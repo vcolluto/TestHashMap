cda86e48d7e062bfb23ae92e8a126cb33d044fb9
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-11-17 11:29:02 +0100

    test scorrimento HashMap e ricerca

---
+++ src/org/generation/italy/Main.java
@@ -0,0 +1,35 @@
+package org.generation.italy;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HashMap<String, String> elencoAuto = new HashMap<>();
+		Scanner sc=new Scanner(System.in);
+		
+		elencoAuto.put("DZ324AB", "Fiat Panda");
+		elencoAuto.put("EP652YU", "Ford Fiesta");
+		elencoAuto.put("CP432PO", "Opel Corsa");
+		elencoAuto.put("EG123RE", "Ford Fiesta");
+		
+		
+		System.out.println("Auto inserite:");
+		for(String t:elencoAuto.keySet())	//scorre tutte le targhe (chiavi)
+			System.out.println(elencoAuto.get(t));
+		
+		System.out.println("Inserisci la targa: ");
+		String targa=sc.nextLine();
+		if (elencoAuto.containsKey(targa))
+			System.out.println("Auto trovata: "+ elencoAuto.get(targa));	//nel .get metto la chiave da cercare
+		else
+			System.out.println("Auto non trovata!");
+		
+		
+		
+		sc.close();
+
+	}
+
+}
